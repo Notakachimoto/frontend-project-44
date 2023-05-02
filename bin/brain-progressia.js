1dcc9b0e9d912e75f5b63f22eca1dba2230a51c6
Platon <slavinskiy03@bk.ru>
2023-05-02 13:00:46 +0300

    sq5

--- bin/brain-progressia.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-import brainProgression from '../src/games/progressia.js';
-
-brainProgression();
--- src/games/progression.js
+++ src/games/progression.js
@@ -1,29 +1,19 @@
 import { getRandomNumber, getRandomIntInclusive } from '../util.js';
-
 import game from '../index.js';
 
 const gameTask = 'What number is missing in the progression?';
 
 const expression = () => {
   const randomSum = getRandomNumber(10);
-
   const randomLength = getRandomIntInclusive(5, 15);
-
   const arrayExpression = Array.from({
-
     length: Math.ceil(randomLength),
-
   }, (_, i) => (i + 1) * randomSum);
-
   const randomItem = arrayExpression[Math.floor(Math.random() * arrayExpression.length)];
-
   arrayExpression[arrayExpression.indexOf(randomItem)] = '..';
-
   const expressionString = arrayExpression.join(' ');
-
   return [expressionString, String(randomItem)];
 };
-
 export default function brainProgression() {
   game(gameTask, expression);
 }
