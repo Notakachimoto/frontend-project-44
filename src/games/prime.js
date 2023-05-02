3a1fb7bba138b1ca7677d0af0c88bb5994693452
Platon <slavinskiy03@bk.ru>
2023-05-02 13:09:11 +0300

    sq6

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,42 +1,35 @@
 import readlineSync from 'readline-sync';
 
+import welcome from '../cli.js';
+
 const isPrime = (num) => {
   if (num <= 1) {
     return false;
   }
-
   for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) {
       return false;
     }
   }
-
   return true;
 };
 
-const playPrimeGame = (name) => {
+const playPrimeGame = () => {
+  const name = welcome();
   console.log('Answer "yes" if given number is prime. Otherwise answer "no".\n');
-
   const roundsCount = 3;
-
   let correctAnswersCount = 0;
 
   for (let i = 0; i < roundsCount; i += 1) {
     const number = Math.floor(Math.random() * 100);
-
     console.log(`Question: ${number}`);
-
     const userAnswer = readlineSync.question('Your answer: ');
-
     const correctAnswer = isPrime(number) ? 'yes' : 'no';
-
     if (userAnswer.toLowerCase() === correctAnswer) {
       console.log('Correct!');
-
       correctAnswersCount += 1;
     } else {
-      console.log(`'${userAnswer}' is wrong answer -_-. Correct answer was '${correctAnswer}'.\nLet's try again ${name}!`);
-
+      console.log(`'${userAnswer}' is wrong answer -_-. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
       return;
     }
   }
@@ -44,8 +37,4 @@ const playPrimeGame = (name) => {
   console.log(`Congratulations, ${name}! You won!\nYour score: ${correctAnswersCount}/${roundsCount}`);
 };
 
-const userName = readlineSync.question('May I have your name? ');
-
-playPrimeGame(userName);
-
-export default isPrime;
+export default playPrimeGame;
